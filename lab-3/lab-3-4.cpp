1b5d01a476184187a1cb7f16f561db258baed1e5
Kristi00iW <kristi0iw0@gmail.com>
2025-11-28 10:53:12 +0300

    first commit

--- lab-3/lab-3-4.cpp
+++ lab-3/lab-3-4.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main() {
+    vector<int> arr = {2, -5, 8, -3, 10, 7, 4, -1};
+
+    // Удаляем первый отрицательный
+    for (size_t i = 0; i < arr.size(); i++) {
+        if (arr[i] < 0) {
+            arr.erase(arr.begin() + i);
+            break;
+        }
+    }
+
+    // Удаляем последний четный
+    for (int i = arr.size() - 1; i >= 0; i--) {
+        if (arr[i] % 2 == 0) {
+            arr.erase(arr.begin() + i);
+            break;
+        }
+    }
+
+    // Вывод результата
+    for (int num : arr) cout << num << " ";
+
+    return 0;
+}
